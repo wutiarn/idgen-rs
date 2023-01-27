ae6beb9d156a1f15b58dadab225df50e5ce77fa7
Dmitry Romanov <me@wtrn.ru>
2023-01-27 10:08:06 +0300

    Setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use env_logger::Target;
+use log::{info, LevelFilter};
 use rocket;
 
 #[rocket::main]
 async fn main() {
+    env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .target(Target::Stdout)
+        .init();
+    info!("Starting idgen-rs");
     let _ = rocket::build().launch().await;
 }
