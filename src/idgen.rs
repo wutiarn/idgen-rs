f09e473c0d767a38ea442fce3b5d554accb9b6b2
Dmitry Romanov <me@wtrn.ru>
2023-01-28 17:44:28 +0300

    WIP: Idgen

--- src/idgen.rs
+++ src/idgen.rs
@@ -1,20 +1,27 @@
 use std::fmt::format;
 use std::sync::Mutex;
+use std::time::{Instant, SystemTime};
+use arr_macro::arr;
 use log::info;
+use once_cell::sync::Lazy;
 
 const TIMESTAMP_BITS: usize = 35;
 const COUNTER_BITS: usize = 14;
 const INSTANCE_ID_BITS: usize = 6;
 const DOMAIN_BITS: usize = 8;
-const RESERVED_SECONDS_COUNT: usize = 10;
 
 const DOMAINS_COUNT: usize = usize::pow(2, DOMAIN_BITS as u32);
+
+static EPOCH_START: Lazy<Instant> = Lazy::new(|| Instant::now());
+const RESERVED_SECONDS_COUNT: usize = 10;
+
 
 struct IdGenerator {
     domain_state_holders: [Mutex<DomainStateHolder>; DOMAINS_COUNT],
 }
 
 struct DomainStateHolder {
+    domain: u8,
     timestamp: u64,
     counter: u64,
 }
@@ -29,29 +36,33 @@ impl IdGenerator {
     }
 
     pub fn create() -> IdGenerator {
-        let mut holders: [Mutex<DomainStateHolder>; DOMAINS_COUNT] = [];
-        for i in 0..DOMAINS_COUNT {
-            let holder = DomainStateHolder {
-                counter: 0,
-                timestamp: 0
-            };
-            holders[i] = Mutex::new(holder)
-        }
+        let mut i = 0u8;
+        let holders: [Mutex<DomainStateHolder>; DOMAINS_COUNT] = arr![DomainStateHolder::new({i}); 256];
         return IdGenerator {
             domain_state_holders: holders,
-        }
+        };
     }
 }
 
 impl DomainStateHolder {
+    pub fn new(domain: u8) -> Mutex<DomainStateHolder> {
+        let holder = DomainStateHolder {
+            domain,
+            counter: 0,
+            timestamp: 0,
+        };
+        Mutex::new(holder)
+    }
     pub fn generate_ids(&self) {
         info!("Generating ids...")
     }
 }
 
+fn get_current_timestamp() {
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
-
 }
