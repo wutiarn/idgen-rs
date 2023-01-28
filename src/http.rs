fc1bc0ce1daec7ee0ffbffff4fd56a379ea4fc0a
Dmitry Romanov <me@wtrn.ru>
2023-01-29 00:07:53 +0300

    Setup rocket /generate endpoint

--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,12 @@
 use rocket;
+use rocket::serde::json::Json;
 use rocket::State;
 use crate::error::HttpError;
 use crate::idgen::IdGenerator;
+use serde::Serialize;
 
 #[rocket::get("/generate?<count>")]
-pub fn generate_ids(count: Option<i32>, id_generator: &State<IdGenerator>) -> Result<String, HttpError> {
+pub fn generate_ids(count: Option<i32>, id_generator: &State<IdGenerator>) -> Result<Json<GenerateIdsResponse>, HttpError> {
     let count = match count {
         Some(c) => c,
         None => 10
@@ -13,5 +15,10 @@ pub fn generate_ids(count: Option<i32>, id_generator: &State<IdGenerator>) -> Re
        return Err(HttpError::BadRequest("count must be greater than 0".into()))
     }
     let ids = id_generator.generate_ids(count as usize, 0);
-    Ok(format!("OK: {count} ids"))
+    Ok(Json(GenerateIdsResponse { ids }))
+}
+
+#[derive(Serialize, Debug)]
+pub struct GenerateIdsResponse {
+    pub ids: Vec<u64>
 }
