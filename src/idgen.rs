cf99991004c55176694a364db9657ee3dcbdc62d
Dmitry Romanov <me@wtrn.ru>
2023-01-28 17:14:13 +0300

    WIP: Idgen

--- src/idgen.rs
+++ src/idgen.rs
@@ -3,10 +3,10 @@ use std::sync::Mutex;
 use log::info;
 
 const TIMESTAMP_BITS: usize = 35;
-const COUNTER_BITS: usize = 35;
-const INSTANCE_ID_BITS: usize = 35;
-const DOMAIN_BITS: usize = 35;
-const RESERVED_SECONDS_COUNT: usize = 35;
+const COUNTER_BITS: usize = 14;
+const INSTANCE_ID_BITS: usize = 6;
+const DOMAIN_BITS: usize = 8;
+const RESERVED_SECONDS_COUNT: usize = 10;
 
 const DOMAINS_COUNT: usize = usize::pow(2, DOMAIN_BITS as u32);
 
@@ -25,6 +25,33 @@ impl IdGenerator {
         let mutex = self.domain_state_holders.get(0)
             .expect(&format!("domain_state_holders should contain state for domain {domain}"));
         let state = mutex.lock().unwrap();
-        info!("Successfully acquired state")
+        state.generate_ids()
     }
+
+    pub fn create() -> IdGenerator {
+        let mut holders: [Mutex<DomainStateHolder>; DOMAINS_COUNT] = [];
+        for i in 0..DOMAINS_COUNT {
+            let holder = DomainStateHolder {
+                counter: 0,
+                timestamp: 0
+            };
+            holders[i] = Mutex::new(holder)
+        }
+        return IdGenerator {
+            domain_state_holders: holders,
+        }
+    }
+}
+
+impl DomainStateHolder {
+    pub fn generate_ids(&self) {
+        info!("Generating ids...")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
 }
